5d362bbb5b497945f83fff73d923a7174c7fb6db
Alford Miller <Big_Al_Doogie@Yahoo.Com>
2024-08-26 19:39:41 -0400

    Made first systems for my first components.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-use bevy::prelude::*;
+mod my_components;
 
-fn hello_worl() {
+mod  prelude {
+    pub use bevy::prelude::*;
+    pub use crate::my_components::*;
+}
+use prelude::*;
+
+fn hello_world() {
     println!("Hello World!");
+}
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, PersonName("Elaina Proctor".to_string())));
+    commands.spawn((Person, PersonName("Renzo Hume".to_string())));
+    commands.spawn((Person, PersonName("Zayna Nieves".to_string())));
+}
+fn greet_people(query: Query<&PersonName, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
 fn main() {
     App::new()
-        .add_systems(Update, hello_world)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
         .run();
 }
---
+++ src/my_components.rs
@@ -0,0 +1,7 @@
+use crate::prelude::*;
+
+#[derive(Component)]
+pub struct Person;
+
+#[derive(Component)]
+pub struct PersonName(pub String);
