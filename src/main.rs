b2f0f9850606ce3f9d1c14761633fa0342ced120
Alford Miller <Big_Al_Doogie@Yahoo.Com>
2024-08-26 21:19:39 -0400

    Added timer to control output of hello messages after adding windows components which causes repeating behavour.  NOTE:  The TimerMode is set to repeating in this example.  Setting TimerMode to Once will limit output to 1 instance.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod my_components;
 mod my_plugins;
+mod my_resources;
 
 mod  prelude {
     pub use bevy::prelude::*;
     pub use crate::my_components::*;
     pub use crate::my_plugins::*;
+    pub use crate::my_resources::*;
 }
 use prelude::*;
 
--- src/my_plugins.rs
+++ src/my_plugins.rs
@@ -3,22 +3,27 @@ use crate::prelude::*;
 pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hello_world, greet_people));
+        app.add_systems(Update, greet_people);
 
     }
 }
 
-pub fn hello_world() {
-    println!("Hello World!");
-}
+// pub fn hello_world(time: Res<Time>, mut timer: ResMut<GreetTimer>) {
+//     if timer.0.tick(time.delta()).just_finished() {
+//         println!("Hello World!");
+//     }
+// }
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, PersonName("Elaina Proctor".to_string())));
     commands.spawn((Person, PersonName("Renzo Hume".to_string())));
     commands.spawn((Person, PersonName("Zayna Nieves".to_string())));
 }
-fn greet_people(query: Query<&PersonName, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&PersonName, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
